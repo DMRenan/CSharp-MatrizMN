b487482ffe381424f7ed6e829f2adf6e12d2f033
Renan Duarte <rnnmrcdrt@gmail.com>
2022-04-18 12:36:15 -0300

    First commit

---
+++ MatrizMN/MatrizMxN.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace MatrizMN
+{
+    internal class MatrizMxN
+    {
+        public int m { get; set; }
+        public int n { get; set; }
+        public int[,] matriz { get; set; }
+
+        public MatrizMxN(int m, int n)
+        {
+            this.m = m;
+            this.n = n;
+            this.matriz = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Insira o valor do elemento [{i}][{j}] : ");
+                    int elemento = int.Parse(Console.ReadLine());
+                    matriz[i, j] = elemento;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"-----Matriz {m}x{n}-----");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int Elemento(int i, int j)
+        {
+            return matriz[i, j];
+        }
+    }
+}
---
+++ MatrizMN/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MatrizMN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de linhas da matriz: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de colunas da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            MatrizMxN matriz = new MatrizMxN(m, n);
+            Console.WriteLine();
+
+            Console.Write("Digite um número para ser procurado na tabela: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz.Elemento(i, j) == numero)
+                    {
+                        Console.WriteLine($"Posição: [{i}][{j}]");
+
+                        if (j > 0) Console.WriteLine($"Esquerda: {matriz.Elemento(i, j - 1)}");
+
+                        if (i > 0) Console.WriteLine($"Acima: {matriz.Elemento(i-1, j)}");
+
+                        if (i< n - 1) Console.WriteLine($"Direita: {matriz.Elemento(i, j+1)}");  
+                        
+                        if (j< m - 1) Console.WriteLine($"Abaixo: {matriz.Elemento(i+1, j)}");
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
